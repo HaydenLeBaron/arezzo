ff85ca8ba4bb372b649063a22c93376ffc8e478f
Hayden LeBaron <hlebaron98@gmail.com>
2021-04-30 10:56:54 -0600

    Turn channel bodies into char lists

--- main.rkt
+++ main.rkt
@@ -1,5 +1,8 @@
 #lang racket
 
+(define (symbol->list sym)
+  (string->list (symbol->string sym)))
+
 (define-syntax-rule
   (composition
    comp-name
@@ -21,24 +24,24 @@
 
   (define comp-name
     (hasheqv
-     gl-tempo gl-tempo-body
-     gl-unitr gl-unitr-body
-     v1-root v1-root-body
-     v2-root v2-root-body
-     v3-root v3-root-body
-     v4-root v4-root-body
-     v1-oct v1-oct-body
-     v2-oct v2-oct-body
-     v3-oct v3-oct-body
-     v4-oct v4-oct-body
-     v1-part v1-part-body
-     v2-part v2-part-body
-     v3-part v3-part-body
-     v4-part v4-part-body
+     gl-tempo (symbol->list gl-tempo-body)
+     gl-unitr (symbol->list gl-unitr-body)
+     v1-root (symbol->list v1-root-body)
+     v2-root (symbol->list v2-root-body)
+     v3-root (symbol->list v3-root-body)
+     v4-root (symbol->list v4-root-body)
+     v1-oct (symbol->list v1-oct-body)
+     v2-oct (symbol->list v2-oct-body)
+     v3-oct (symbol->list v3-oct-body)
+     v4-oct (symbol->list v4-oct-body)
+     v1-part (symbol->list v1-part-body)
+     v2-part (symbol->list v2-part-body)
+     v3-part (symbol->list v3-part-body)
+     v4-part (symbol->list v4-part-body)
      )))
 
 
-
+;; arezzo lite. Defines a composition named "sonata"
 (composition
  sonata
  'gl-tempo 'X---Y-----
@@ -56,4 +59,10 @@
  'v3-part '5==5543===
  'v4-part '1==5=5==.-
  )
+
+;; TODO
+(define (char->tempo-event)
+  (void))
+
+
 
