442a0db88c40a59f9ec8d11e59252808b59d4b12
Hayden LeBaron <hlebaron98@gmail.com>
2021-04-30 10:48:50 -0600

    Allow channel keys to be named

--- main.rkt
+++ main.rkt
@@ -3,58 +3,57 @@
 (define-syntax-rule
   (composition
    comp-name
-   (gl-tempo gl-tempo-body)
-   (gl-unitr gl-unitr-body)
-   (v1-root v1-root-body)
-   (v2-root v2-root-body)
-   (v3-root v3-root-body)
-   (v4-root v4-root-body)
-   (v1-oct v1-oct-body)
-   (v2-oct v2-oct-body)
-   (v3-oct v3-oct-body)
-   (v4-oct v4-oct-body)
-   (v1-part v1-part-body)
-   (v2-part v2-part-body)
-   (v3-part v3-part-body)
-   (v4-part v4-part-body)
+   gl-tempo gl-tempo-body
+   gl-unitr gl-unitr-body
+   v1-root v1-root-body
+   v2-root v2-root-body
+   v3-root v3-root-body
+   v4-root v4-root-body
+   v1-oct v1-oct-body
+   v2-oct v2-oct-body
+   v3-oct v3-oct-body
+   v4-oct v4-oct-body
+   v1-part v1-part-body
+   v2-part v2-part-body
+   v3-part v3-part-body
+   v4-part v4-part-body
   )
 
   (define comp-name
     (hasheqv
-     'a-gl-tempo gl-tempo-body
-     'a-gl-unitr gl-unitr-body
-     'a-v1-root v1-root-body
-     'a-v2-root v2-root-body
-     'a-v3-root v3-root-body
-     'a-v4-root v4-root-body
-     'a-v1-oct v1-oct-body
-     'a-v2-oct v2-oct-body
-     'a-v3-oct v3-oct-body
-     'a-v4-oct v4-oct-body
-     'a-v1-part v1-part-body
-     'a-v2-part v2-part-body
-     'a-v3-part v3-part-body
-     'a-v4-part v4-part-body
+     gl-tempo gl-tempo-body
+     gl-unitr gl-unitr-body
+     v1-root v1-root-body
+     v2-root v2-root-body
+     v3-root v3-root-body
+     v4-root v4-root-body
+     v1-oct v1-oct-body
+     v2-oct v2-oct-body
+     v3-oct v3-oct-body
+     v4-oct v4-oct-body
+     v1-part v1-part-body
+     v2-part v2-part-body
+     v3-part v3-part-body
+     v4-part v4-part-body
      )))
 
-;; 
+
+
 (composition
  sonata
- (gl-tempo 'X---Y-----)
- (gl-unitr '4---3-----)
- (v1-root 'c---0---d-)
- (v2-root 'c---------)
- (v3-root 'c---------)
- (v4-root 'c---------)
- (v1-oct '4----5----)
- (v2-oct '4---------)
- (v3-oct '3---------)
- (v4-oct '3---------)
- (v1-part '1!=2.-3===)
- (v2-part '3=@55-1===)
- (v3-part '5==5543===)
- (v4-part '1==5=5==.-)
+ 'gl-tempo 'X---Y-----
+ 'gl-unitr '4---3-----
+ 'v1-root 'c---0---d-
+ 'v2-root 'c---------
+ 'v3-root 'c---------
+ 'v4-root 'c---------
+ 'v1-oct '4----5----
+ 'v2-oct '4---------
+ 'v3-oct '3---------
+ 'v4-oct '3---------
+ 'v1-part '1!=2.-3===
+ 'v2-part '3=@55-1===
+ 'v3-part '5==5543===
+ 'v4-part '1==5=5==.-
  )
-
-
 
